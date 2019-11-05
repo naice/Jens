5aeacf24dc5d0741e58fc605116011b84e0b036e
Jens Marchewka <jens.marchewka@protel.net>
2019-11-05 13:57:45 +0100

    Aspect Oriented Authorization Test.

---
+++ Jens.RestServer.Sample/AspectOrientedAuthorizationApiTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jens.RestServer.Sample
+{
+    [RestServerServiceInstance(RestServerServiceInstanceType.Instance)]
+    public class AspectOrientedAuthorizationApiTest : RestServerService
+    {
+
+    }
+}
---
+++ Jens.RestServer.Sample/AspectOrientedAuthorizationAttributeInterceptor.cs
@@ -0,0 +1,54 @@
+﻿using Jens.InversionOfControl;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jens.RestServer.Sample
+{
+    [System.AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class AuthorizationAttribute : Attribute
+    {
+        // See the attribute guidelines at 
+        //  http://go.microsoft.com/fwlink/?LinkId=85236
+        readonly string[] _rights;
+
+        // This is a positional argument
+        public AuthorizationAttribute(params string[] rights)
+        {
+            _rights = rights;
+        }
+
+        public string[] Rights
+        {
+            get { return _rights; }
+        }
+    }
+
+    public class UnauthorizedException : RestServerServiceCallException
+    {
+        public UnauthorizedException(string message) : base(401, "Unauthorized", message)
+        {
+            
+        }
+    }
+
+    public class AspectOrientedAuthorizationAttributeInterceptor : AttributeInterceptor<AuthorizationAttribute>
+    {
+        private readonly IReadOnlyList<string> _givenRights;
+
+        public AspectOrientedAuthorizationAttributeInterceptor()
+        {
+            _givenRights = new List<string>() {
+                "TestRecht01",
+                "TestRecht02",
+                "TestRecht03",
+            };
+        }
+
+        public override void Intercept(IInvocation invocation, AuthorizationAttribute attribute)
+        {
+            if (!HasRights(attribute.Rights))
+                throw new UnauthorizedException();
+        }
+    }
+}
